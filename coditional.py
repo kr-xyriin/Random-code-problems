f581a9fac79e5689e3a6ec8c64d5a64086430c91
🅳🅾🅾🅼 🅴 🅳 (卂卂D丨ㄒㄚ卂) <aschauhan8941@gmail.com>
2026-02-08 23:02:21 +0530

    Delete coditional.py

--- coditional.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter first number: "))
-b=int(input("Enter seond number: "))
-c=int(input("Enter third number: "))
-if a<b and a<c:
-    print("Smallest number is:", a)
-elif b<c:
-    print("Smallest number is:", b)
-else:
-    print("Smallest number is:", c)
