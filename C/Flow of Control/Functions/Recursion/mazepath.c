b3fb8930d4c06063f5f7eccb7574df8ce5b964db
kr-xyriin <aschauhan8941@gmail.com>
2026-03-15 15:54:12 +0530

    Maze path

---
+++ C/Flow of Control/Functions/Recursion/mazepath.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int maze(int cr, int cc, int er, int ec);
+
+int main() {
+    int n, m, waysNo;
+    printf("Enter no. of rows: ");
+    scanf("%d", &n);
+    printf("Enter no. of columns: ");
+    scanf("%d", &m);
+    waysNo=maze(1, 1, n, m);
+    printf("Number of ways: %d", waysNo);
+    return 0;
+}
+
+int maze(int cr, int cc, int er, int ec) {
+    int rightways=0;
+    int downways=0;
+    if (cr==er && cc==ec) {
+        return 1;
+    }
+    if (cr==er) {
+        rightways+=maze(cr, cc+1, er, ec);
+    }
+    if (cc==ec) {
+        downways+=maze(cr+1, cc, er, ec);
+    }
+    if (cr<er && cc<ec) {
+        rightways+=maze(cr+1, cc, er, ec);
+        downways+=maze(cr, cc+1, er, ec);
+    }
+    int totalways=rightways+downways;
+    return totalways;
+}
