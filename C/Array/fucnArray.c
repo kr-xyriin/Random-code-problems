6a03426c7131395857bb72c90bb360944249100b
kr-xyriin <aschauhan8941@gmail.com>
2026-03-18 12:12:17 +0530

    Experiment 7 in python lab

--- C/Array/fucnArray.c
+++ C/Array/fucnArray.c
@@ -1,9 +1,12 @@
+// Arrays in a fucntion are PASS BY REFERENCE type 
+
 #include <stdio.h>
 
 void fun(int a[]);
 
 int main() {
     int arr[5]={1,2,3,4,5};
+    printf("Value before [arr[0]]: %d", arr[0]);
     fun(arr);
     return 0;
 }
@@ -12,4 +15,6 @@ void fun(int a[]) {
     for (int i=0; i<5; i++) {
         printf("%d\n", a[i]);
     }
+    a[0]=109;
+    printf("Updating value of array inside function [arr[0]]: %d", a[0]);
 }
