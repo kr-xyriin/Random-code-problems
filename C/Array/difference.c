e8974cb4d9f8784d3e569b5b06c0fe0c1711cd70
kr-xyriin <aschauhan8941@gmail.com>
2026-03-18 12:50:55 +0530

    Updated a variable

--- C/Array/difference.c
+++ C/Array/difference.c
@@ -5,7 +5,7 @@ Find the difference between the sum of elements at even indices to the sum ot el
 #include <stdio.h>
 
 int main() {
-    int arr[6]={1,3,5,7,9,11}, diff=0, sum_even=0, sum_odd=0;
+    int arr[6]={1,3,5,7,9,11}, sum_even=0, sum_odd=0;
     for (int i=0; i<6; i++) {
         if (i%2==0) sum_even+=arr[i];
         else sum_odd+=arr[i];
