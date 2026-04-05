80ad0aec438514df68688711b1c7f6cee76a5556
kr-xyriin <aschauhan8941@gmail.com>
2026-04-05 20:28:17 +0530

    Transpose of a matrix

---
+++ C/Array/2-D array/Transpose.c
@@ -0,0 +1,22 @@
+// Program to print the transpose of a matrix
+
+#include <stdio.h>
+
+int main() {
+    int arr[3][5]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, arrT[5][3];
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<5; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int j=0; j<5; j++) {
+        for (int i=0; i<3; i++) {
+            printf("%d ", arr[i][j]);
+            arrT[j][i]=arr[i][j];
+        }
+        printf("\n");
+    }
+    return 0;
+}
