ec5ccf7afa6615c9cd81c272dbf4ae03092375c4
kr-xyriin <aschauhan8941@gmail.com>
2026-04-06 06:59:32 +0530

    Transpose of any matrix matrix including square matrices

--- C/Array/2-D array/Transpose.c
+++ C/Array/2-D array/Transpose.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 
 int main() {
-    int arr[3][5]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}, arrT[5][3];
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<5; j++) {
+    int arr[4][4]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}, arrT[5][3];
+    for (int i=0; i<4; i++) {
+        for (int j=0; j<4; j++) {
             printf("%d ", arr[i][j]);
         }
         printf("\n");
     }
     printf("\n");
-    for (int j=0; j<5; j++) {
-        for (int i=0; i<3; i++) {
+    for (int j=0; j<4; j++) {
+        for (int i=0; i<4; i++) {
             printf("%d ", arr[i][j]);
             arrT[j][i]=arr[i][j];
         }
