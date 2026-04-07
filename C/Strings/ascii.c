a1c4909398cb0247427de54e2ee1d1df3c768157
kr-xyriin <aschauhan8941@gmail.com>
2026-04-07 07:35:12 +0530

    Ascii values of different characters in c

---
+++ C/Strings/ascii.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char ch1='Z';
+    int x1 = (int) ch1;
+    printf("%c\n", ch1);
+    printf("%d\n", ch1);
+    printf("%d\n", x1);
+    
+    char ch2='1';
+    int x2=(int)ch2;
+    printf("%c\n", ch2);
+    printf("%d\n", ch2);
+    printf("%d\n", x2);
+    return 0;
+}
