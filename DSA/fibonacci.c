f4c4cfc0de68c7b5bce0b015d2f5374e9a2707c6
kr-xyriin <aschauhan8941@gmail.com>
2026-04-13 23:40:08 +0530

    algorithm in c to print 20 fibonacci numbers

---
+++ DSA/fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void fibonacci(int num1, int num2, int count);
+
+int main() {
+    int num1=0, num2=1, count=2;
+    printf("%d\n%d\n", num1, num2, count);
+    fibonacci(num1, num2, count);
+    return 0;
+}
+
+void fibonacci(int num1, int num2, int count) {
+    if (count<=19) {
+        int fibo=num1+num2;
+        printf("%d\n", fibo);
+        num1=num2;
+        num2=fibo;
+        count++;
+        fibonacci(num1, num2, count);
+    }
+    return;
+}
