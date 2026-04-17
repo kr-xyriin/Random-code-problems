958306dbd2852f4f17016eb24c80b1f64369953b
kr-xyriin <aschauhan8941@gmail.com>
2026-04-17 23:42:58 +0530

    Further into structures

--- C/Structures/bookStructure.c
+++ C/Structures/bookStructure.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     struct book{
@@ -9,6 +10,19 @@ int main() {
 
     a.numberofPages=100;
     a.price=411.5;
+    strcpy(a.name, "Secret Seven");
+
+    printf("%d\n", a.numberofPages);
+    printf("%.2f\n", a.price);
+    printf("%s\n", a.name);
+
+    b.numberofPages=200;
+    b.price=100.4;
+    strcpy(b.name, "Famous five");
+
+    printf("%d\n", b.numberofPages);
+    printf("%.2f\n", b.price);
+    printf("%s\n", b.name);
 
     return 0;
 }
