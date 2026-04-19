a85c44da3a2c3ce21b74a04a1e40092342a15b6b
kr-xyriin <aschauhan8941@gmail.com>
2026-04-19 10:28:21 +0530

    strcpy in strings

--- C/Strings/builtin.c
+++ C/Strings/builtin.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <strings.h>
 
 int main() {
-    
+    char name[50];
+    printf("Enter a string: ");
+    strcpy(name, gets(name));
+
+    puts(name);
     return 0;
 }
