df7cc7d7620916a513eb334476ed998c437cf967
kr-xyriin <aschauhan8941@gmail.com>
2026-04-24 12:34:52 +0530

    Multiple and Multi-level inheritance in OOPs

---
+++ Python/OOPs/bro Code/Inheritence/info.md
@@ -0,0 +1,17 @@
+# Inheritance
+
+## Multiple inheritance
+
+A class can inherit from more than one parent classes </br>
+</br>
+
+_Syntax: ```class <class_name>(parent_class-1, parent_class-2, and so on...)```_ </br>
+`class C(A, B)`
+</br>
+
+## Multilevel inheritance
+
+A class can inherit from a parent class which would be a child class to another parent class
+</br>
+
+_Syntax:`C(B)<-B(A)<-A`_
---
+++ Python/OOPs/bro Code/Inheritence/intro.py
@@ -0,0 +1,32 @@
+class Animal:
+    def __init__(self, name):
+        self.name=name
+        self.is_alive=True
+
+    def eat(self):
+        print(f"{self.name} is eating")
+    
+    def sleep(self):
+        print(f"{self.name} is asleep")
+    
+class Dog(Animal):
+    def speak(self):
+        print("Woof")
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW")
+
+class Mouse(Animal):
+    def speak(self):
+        print("SQWEEK!!")
+
+dog=Dog("Scooby")
+cat=Cat("Garfield")
+mouse=Mouse("Mickey")
+
+print(dog.name)
+print(dog.is_alive)
+dog.eat()
+dog.sleep()
+dog.speak()
