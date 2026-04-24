12571d66b95062e8b1fe9c7e6e092545aa4b3ceb
kr-xyriin <aschauhan8941@gmail.com>
2026-04-24 11:36:21 +0530

    Intro to OOPs in python

---
+++ Python/OOPs/bro Code/intro/method.py
@@ -0,0 +1,9 @@
+from car import Car
+
+car1=Car("Mustang", 2024, "Red", False)
+car2=Car("Covette", 2025, "Blue", True)
+car3=Car("Charger", 2026, "Yellow", True)
+
+print()
+car3.describe()
+print()
