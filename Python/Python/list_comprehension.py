f6038a067cce5a3e22cb1459f3a03acfcac8333c
kr-xyriin <aschauhan8941@gmail.com>
2026-04-24 23:09:29 +0530

    List comprehension in python

---
+++ Python/Python/list_comprehension.py
@@ -0,0 +1,8 @@
+doubles=[x*2 for x in range(1,11) if x%2==0]
+print(doubles)
+print()
+
+fruits=['apple', 'orange', 'banana', 'coconut']
+upper_fruits=[fruit[0] for fruit in fruits]
+print(upper_fruits)
+print()
