09523ca63a6b5adcd0018721671e96dd136cab4d
kr-xyriin <aschauhan8941@gmail.com>
2026-04-29 19:55:46 +0530

    null pointers in c

--- DSA/Pointers/nullPointer.c
+++ DSA/Pointers/nullPointer.c
@@ -4,8 +4,8 @@ int main() {
     int x=10;
     int *ptr1=NULL;
     int *ptr2=0;
-    printf("%d\n%d\n", *ptr1, *ptr2);
+    printf("%p\n%p\n", *ptr1, *ptr2);
     void *ptr3=NULL;
-    printf("%d\n", *(int*)ptr3);
+    printf("%u\n", *(int*)ptr3);
     return 0;
 }
