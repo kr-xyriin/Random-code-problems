737808545b54bef96d59314d16d0a4637818bcbc
kr-xyriin <aschauhan8941@gmail.com>
2026-05-01 06:56:21 +0530

    Abstraction and polymorphism in python

--- Python/OOPs/bro Code/Polymorphism/intro.py
+++ Python/OOPs/bro Code/Polymorphism/intro.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 
-class Shape:
+class Shape(ABC):
     @abstractmethod
-    def area(self):
+    def area():
         pass
 
 class Circle(Shape):
@@ -23,7 +23,7 @@ class Triangle(Shape):
     def __init__(self, base, height):
         self.base=base
         self.height=height
-    
+
     def area(self):
         return self.base*self.height*0.5
 
@@ -31,9 +31,8 @@ class Pizza(Circle):
     def __init__(self, topping, radius):
         super().__init__(radius)
         self.topping=topping
-        
 
-shapes=[Circle(7), Square(4), Triangle(3,4), Pizza("Pepperoni", 15)]
+shapes=[Circle(4), Square(5), Triangle(6,7), Pizza("Pepperoni",15)]
 
 for shape in shapes:
     print(shape.area())
@@ -45,5 +44,5 @@ and a food
 
 therefore it has three forms
 
-athis is polymorphism i.e. A class tht=at has multiple forms
+this is polymorphism i.e. A class that has multiple forms
 '''
