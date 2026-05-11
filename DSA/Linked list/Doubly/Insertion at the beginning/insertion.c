dadc57be333af1a897ae3431615cdfd7fa04463b
kr-xyriin <aschauhan8941@gmail.com>
2026-05-11 16:42:24 +0530

    Insertion at the beginning and end of doubly linked list

---
+++ DSA/Linked list/Doubly/Insertion at the beginning/insertion.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *prev;
+    struct node *next;
+} node;
+
+node *new(int data) {
+    node *temp=malloc(sizeof(node));
+    temp->data=data;
+    temp->prev=NULL;
+    temp->next=NULL;
+    return temp;
+}
+
+node *insert_atBeginning(node *head, int data) {
+    node *temp=new(data);
+    head->prev=temp;
+    temp->next=head;
+    return temp;
+}
+
+void display(node *head) {
+    if (!head) {
+        printf("Empty list...\n");
+        exit(0);
+    }
+    while(head) {
+        printf("%d -> ", head->data);
+        head=head->next;
+    }
+    printf("NULL\n");
+    return;
+}
+
+int main() {
+    int n, data;
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    node *head=NULL;
+    node *tail=NULL;
+    node *temp=NULL;
+    for (int i=0; i<n; i++) {
+        printf("Enter data: ");
+        scanf("%d", &data);
+        temp=new(data);
+        if (!head) head=tail=temp;
+        else {
+            tail->next=temp;
+            temp->prev=tail;
+            tail=temp;
+        }
+    }
+    display(head);
+    printf("\nEnter data: ");
+    scanf("%d", &data);
+    head=insert_atBeginning(head, data);
+    display(head);
+    return 0;
+}
