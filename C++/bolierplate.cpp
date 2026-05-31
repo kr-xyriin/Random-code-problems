167403cb4c20132d8319f4947581b6b19fd201ea
kr-xyriin <aschauhan8941@gmail.com>
2026-05-31 22:54:59 +0530

    intro to C++

---
+++ C++/bolierplate.cpp
@@ -0,0 +1,9 @@
+// Below is a boilerplate code for c++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
---
+++ C++/intro.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Yoyii\n"<<endl;
+    cout<<"Myself" << endl << "Aaditya Singh Chauhan\n";
+    return 0;
+}
