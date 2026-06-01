2d55d4bfc41d6e0d058e9ccfd7ba24a32362cb33
kr-xyriin <aschauhan8941@gmail.com>
2026-06-01 16:33:16 +0530

    implicit type conversion in c++

---
+++ C++/typeConversion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a='a';
+    char A='A';
+    int asciia=a;
+    int asciiA=A;
+    cout<<asciia<<endl;
+    cout<<asciiA<<endl;
+    return 0;
+}
