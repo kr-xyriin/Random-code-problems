b5e7afaa7bc23d7a9dd05e99e60d0ca77d7f7cf1
kr-xyriin <aschauhan8941@gmail.com>
2026-06-01 17:44:25 +0530

    ternary operator in cpp

---
+++ C++/ternary.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    num>0? cout<<"Positive\n":cout<<"Negative\n";
+    return 0;
+}
