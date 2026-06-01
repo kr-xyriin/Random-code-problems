5a9ff26ec6be0dd26d9a823abb12edd19aa73a30
kr-xyriin <aschauhan8941@gmail.com>
2026-06-01 17:05:09 +0530

    operators in c++

--- C++/operators.cpp
+++ C++/operators.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main() {
+    // arithmetic operator 
     int a=5, b=2;
     float div=a/(float)b;
     cout<<div<<endl;
+
+    //relational operators [return true[1] or false[0]]
+    cout<<(3<5)<<endl;
+
+    // logical operators
+    cout<<(true || true)<<endl;
+    cout<<(false || true) <<endl;
+    cout<<(not true) <<endl;
+    cout<<(true and true)<<endl;
     return 0;
 }
