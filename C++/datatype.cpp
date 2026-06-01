6ad5957d83ae78fef5ed7993cd5ef7098ab17084
kr-xyriin <aschauhan8941@gmail.com>
2026-06-01 16:31:59 +0530

    datatypes in c++ and their representation

---
+++ C++/datatype.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a=20;
+    char b='a';
+    float c=20.5f;
+    bool d=true;
+    double e=23.42;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<"is true? "<<d<<endl;
+    cout<<e<<endl;
+    return 0;
+}
