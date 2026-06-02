9f53ba7c793aa57ee1d11f2022c84cec0ce38817
kr-xyriin <aschauhan8941@gmail.com>
2026-06-02 11:25:04 +0530

    sqaure type patterns

---
+++ C++/Patterns/square/chain.cpp
@@ -0,0 +1,23 @@
+/*
+123
+456
+789
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, num=1;
+    cout<<"Enter limit: ";
+    cin>>n;
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
