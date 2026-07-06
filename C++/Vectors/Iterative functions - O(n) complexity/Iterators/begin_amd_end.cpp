044a4f642f9ead416ec90be7f951635188561fc0
kr-xyriin <aschauhan8941@gmail.com>
2026-07-06 17:17:53 +0530

    Begin and end functions

---
+++ C++/Vectors/Iterative functions - O(n) complexity/Iterators/begin_amd_end.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> vec={1,2,3,4,5,6,7,8,9,0};
+    cout<<*vec.begin()<<endl;
+    cout<<*vec.end()<<endl;     // Points just past the last index i.e. prints a garbage value
+    cout<<*(vec.end()-1)<<endl;     // Points at the last index
+    // cout<<vec.back()<<endl;
+    return 0;
+}
