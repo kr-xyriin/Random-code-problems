6ca782620431a175896bbae37d31a5f7b8ac63bf
kr-xyriin <aschauhan8941@gmail.com>
2026-07-21 04:41:04 +0530

    INduced tester file

--- C++/Vectors/tester.cpp
+++ C++/Vectors/tester.cpp
@@ -11,26 +11,8 @@ void display(vector<int> vec) {
 
 int main() {
     vector<int> vec={1,2,3,4,5,6,7,8,9,0};
-    cout<<"Current vector: ";
-    display(vec);
-    cout<<endl;
-    cout<<"First element: "<<vec.front()<<endl;
-    cout<<"Last element: "<<vec.back()<<endl;
-    cout<<"Add an element to last: ";
-    vec.push_back(-1);
-    vec.emplace_back(-2);
-    cout<<endl<<"Current vector: ";
-    display(vec);
-    cout<<endl;
-    cout<<"Remove last element: "<<endl;
-    vec.pop_back();
-    cout<<endl<<"Current vector: ";
-    display(vec);
-    cout<<endl;
-    cout<<"View the value stored in an index: "<<endl;
-    display(vec);
-    cout<<"At index 2: "<<vec[2]<<endl;
-    cout<<"At index 8: "<<vec.at(8)<<endl;
-    
+    vec[0]=vec[9];
+    cout<<vec[0]<<endl;
+    cout<<vec[9]<<endl;    
     return 0;
 }
